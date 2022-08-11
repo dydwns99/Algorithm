f3e0bbcdee35b288c2173604369fc5cf72bb1e49
dydwns99 <dydwns2397@gmail.com>
2022-08-11 13:05:07 +0900

    B_1151 메모리 46720KB 시간 564ms 다른 풀이 메모리 14200KB 시간 164ms... 방법은...?

--- src/B_1157.java
+++ src/B_1157.java
@@ -1,7 +1,34 @@
+import java.util.HashMap;
 import java.util.Scanner;
 public class B_1157 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        word = word.toUpperCase();
+        char[] arr = word.toCharArray();
 
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int i = 'A'; i <= 'Z'; i++) {
+            map.put((char) i, 0);
+        }
+        for (int j = 0; j < arr.length; j++) {
+            map.put(arr[j],map.get(arr[j])+1);
+        }
+        int max = 0;
+        char output=' ';
+        for (int k = 'A'; k <= 'Z'; k++) {
+            if (map.get((char)k) > max) {
+                max = map.get((char)k);
+                output = (char)k;
+            }
+        }
+
+        for (int l = 'A'; l <= 'Z'; l++) {
+            if (map.get((char)l) == max && l != output) {
+                output = '?';
+            }
+        }
+
+        System.out.print(output);
     }
 }
