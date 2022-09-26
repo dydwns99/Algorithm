0036a4ae4eeba2b2f0368b2546b986fb53a31ae1
dydwns99 <dydwns2397@gmail.com>
2022-09-26 19:56:50 +0900

    메모리 14532KB / 시간 148ms / 코드길이 1647B 스택 이용함 /

---
+++ JAVA/200_자료구조1/괄호(B_9012).java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.Stack;
+
+public class B_9012 {
+    public static void main(String[] args) throws IOException {
+        //스택으로 (,) 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            Stack<Character> stack = new Stack<>();
+            char[] c = br.readLine().toCharArray();
+            for (int i = 0; i < c.length; i++) {
+                stack.push(c[i]);
+            }
+            int n = stack.size();
+            int m = stack.size();
+            int a=0;
+            int b=0;
+            String s = "YES";
+            //스택 맨위 부터 꺼내 확인
+            while (n -- >0) {
+                if (stack.peek().equals(')')) {
+                    stack.pop();
+                    a++;
+                } else if (stack.peek().equals('(') && n<m) {
+                    b++;
+                    if (b > a) {
+                        s = "NO";
+                        break;
+                    } else {
+                        stack.pop();
+                    }
+                } else if (stack.peek().equals('(') && n == stack.size()) {
+                    s = "NO";
+                    break;
+                }
+            }
+            if (a != b) {
+                s = "NO";
+            }
+            bw.write(s+"\n");
+            bw.flush();
+        }
+        bw.close();
+        br.close();
+        // 1. ) 이면 -> 그다음꺼 ( 혹은 )
+        // 2. ( 이면 NO
+        // 3. 1,2 반복
+    }
+}
+
