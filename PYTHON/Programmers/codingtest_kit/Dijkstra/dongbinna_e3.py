3b09f05cea0a25e9b8478b60ff2b6e045e8fcd7c
kwon-yongjun <dydwns2397@gmail.com>
2023-03-06 12:02:50 +0900

    study dijkstra

--- PYTHON/Programmers/codingtest_kit/Dijkstra/dongbinna_e3.py
+++ PYTHON/Programmers/codingtest_kit/Dijkstra/dongbinna_e3.py
@@ -1,40 +1,31 @@
 # 다익스트라 알고리즘 :  개선된
 import heapq
-import sys
-input = sys.stdin.readline
-INF = int(1e9)
 
+INF = int(1e9) # 무한 의미
+# n : 노드의 개수 , m : 간선의 개수 
 n, m = map(int, input().split())
+# start : 시작 노드
 start = int(input())
-
-graph=[[] for i in range(n+1)]
-
-# visited = [False] * (n+1)
-
+# 각 노드에 연결되어 있는 노드에 대한 정보 담는 리스트
+graph=[[] for i in range(n+1)] # node 1 ~ node n
+# 최단 거리 테이블을 모두 무한으로 초기화
 distance = [INF] * (n+1)
-
+# graph[해당 노드].[연결노드, 거리]
 for _ in range(m):
     a,b,c = map(int, input().split())
     graph[a].append((b,c))
 
 def dijkstra(start):
-    q=[]
-    heapq.heappush(q,(0,start)) # O(lonN)
+    hq=[]
     distance[start]=0
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now]<dist:
+    heapq.heapify(hq, (0, start))
+    while hq:
+        #dist : now 노드가 보유한 거리
+        dist, now = heapq.heappop(hq)
+        if distance[now] < dist:
             continue
         for i in graph[now]:
-            cost = dist + i[1]
-            if cost<distance[i[0]]:
-                distance[i[0]]=cost
-                heapq.heappush(q,(cost,i[0]))
-
-dijkstra(start)
-
-for i in range(1,n+1):
-    if distance[i]==INF:
-        print("INFINITY")
-    else:
-        print(distance[i])
+            cost = i[1] + dist
+            if distance[i[0]] > cost:
+                distance[i[0]] = cost
+            heapq.heappush(hq, ( cost, i[0]))
