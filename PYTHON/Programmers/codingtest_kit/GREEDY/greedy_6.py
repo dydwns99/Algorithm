eddff2250c3f310223662d77f5ad29d6308a0d05
kwon-yongjun <dydwns2397@gmail.com>
2023-03-08 20:52:06 +0900

    complete greedy_6.py

--- PYTHON/Programmers/codingtest_kit/GREEDY/greedy_6.py
+++ PYTHON/Programmers/codingtest_kit/GREEDY/greedy_6.py
@@ -1,33 +1,31 @@
-
+# 단속 카메라 
 def solution(routes):
     answer=0
-    roads = sorted(routes, key= lambda x : x[0])
-    # print(roads)
-    # 다 더해
-    count=0
-    while roads:
-        cam_loc=dict()
-        for i in range(roads[0][0], roads[len(roads)-1][1]+1):
-            cam_loc[str(i)]=0
-
-        group = roads
-        for road in roads:
-            for loc in range(road[0], road[1]+1):
-                cam_loc[str(loc)]+=1
-        mxidx = max(cam_loc, key=cam_loc.get)
-        #가장 높은 수를 빼 -> 그 인덱스에 속한 모든 routes는 뺌 
-        # print(mxidx)
-        arr=[]
-        for road in roads:
-            if int(mxidx) in range(road[0], road[1]+1):
-                arr.append(road)
-                # print(road)
-        for r in arr:
-            roads.remove(r)
-        # print(roads)
-        count+=1
-    # print(count)
-    answer=count
+    routes.sort(key=lambda x:x[1])
+    # print(routes)
+    leng = len(routes)
+    check =[0]*leng
+    
+    for i in range(leng):
+        # 아직 확인을 안했다면
+        if check[i]==0: 
+            cam=routes[i][1] # 카메라는 고속도로의 끝에 위치
+            answer+=1 # 설치했으므로 카운트 해줌
+        for j in range(i+1, leng): # 그 다음 고속도로부터
+            if check[j]==0 and routes[j][0]<=cam <=routes[j][1]: # 아직 확인을 안했고 카메라가 그 고속도로 사이에 있다면
+                check[j]=1 #확인 체크를 함
     return answer
+
+# 
+# def solution(routes):
+#     answer = 0
+#     routes.sort(key=lambda x: x[1]) # routes를 차량이 나간 지점 (진출) 기준으로 정렬
+#     camera = -30001 # -30001부터 카메라 위치를 찾습니다.
+
+#     for route in routes:
+#         if camera < route[0]:
+#             answer += 1
+#             camera = route[1]
+#     return answer
 
 print(solution([[-20,-15], [-14,-5], [-18,-13], [-5,-3]]))
