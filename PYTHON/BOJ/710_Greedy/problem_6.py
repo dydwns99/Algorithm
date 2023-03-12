43311f23f5b29e1a364a7e093283b2513702cfff
kwon-yongjun <dydwns2397@gmail.com>
2023-03-12 11:38:26 +0900

    complete 710_Greedy 1202번 보석도둑

--- PYTHON/BOJ/710_Greedy/problem_6.py
+++ PYTHON/BOJ/710_Greedy/problem_6.py
@@ -1,51 +1,25 @@
-# 1202번 보석 도둑
+import sys
 import heapq
-# N : 보석 개수  K : 가방 개수
-n, k = map(int, input().split())
-# M : 각 보석의 무게  V : 각 보석의 가격 [M, V]
-stones=[]
-for i in range(n):
-    m, v = map(int,input().split())
-    stones.append((m, v))
-# 가격별로 정리하고
-stones.sort(key = lambda x : x[1], reverse=True)
-# 가방의 최대 무게
+
+n, k = map(int,sys.stdin.readline().split())
+jew=[]
+for _ in range(n):
+    heapq.heappush(jew, list(map(int, sys.stdin.readline().split())))
 bags=[]
-for i in range(k):
-    weight = int(input())
-    bags.append(weight)
+for _ in range(k):
+    bags.append(int(sys.stdin.readline()))
+bags.sort()
 
-# print("stones : ",stones)
-price=0
-sum=0
-# print("반복문 시작")
-for st in stones:
-    tmp_bag=[]
-    heapq.heapify(bags)
-    count=len(bags)
-    # print("bags : ", bags)
-    while count!=0:
-        count-=1
-        bag = heapq.heappop(bags)
-        # print("bag : ", bag)
-        # 처음으로 가방에 보석이 들어가면 그 가방은 다시 쓰지 않고 반복문 나옴
-        if bag>=st[0]:
-            sum+=st[1]
-            # 담지 못했던 가방들을 다시 bags 배열에 넣음
-            bags = bags + tmp_bag
-            break
-        # 가방이 보석을 담지 못한다면
-        # 다시 꺼낸 가방을 넣음
-        else:
-            if count==0:
-                bags=tmp_bag
-                break
-            tmp_bag.append(bag)
-        # 모든 가방을 다 확인해 봤다면 반복문 나옴
-        
-    # print("----")
-        
-print(sum)
+answer=0
 
-
-
+tmp_jew=[]
+# 가방을 확인
+for bag in bags:
+    # 보석이 아직 있고 가방이 작은거 부터 그 보석을 담을 수 있으면
+    while jew and bag>=jew[0][0]:
+        # 일시 보석 보관함에 음수 붙여서 가격을 넣음 -> 가격 가장 큰 것이 가장 앞으로 감
+        heapq.heappush(tmp_jew, -heapq.heappop(jew)[1])
+    if tmp_jew:
+        answer -=heapq.heappop(tmp_jew)
+    elif not jew:
+        break
