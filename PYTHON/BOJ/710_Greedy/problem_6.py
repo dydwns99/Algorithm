cbc69d79f900a4436071e60a206a66a5ea88e647
kwon-yongjun <dydwns2397@gmail.com>
2023-03-12 11:27:39 +0900

    try 710_Greedy/problem_6.py

--- PYTHON/BOJ/710_Greedy/problem_6.py
+++ PYTHON/BOJ/710_Greedy/problem_6.py
@@ -1,4 +1,5 @@
 # 1202번 보석 도둑
+import heapq
 # N : 보석 개수  K : 가방 개수
 n, k = map(int, input().split())
 # M : 각 보석의 무게  V : 각 보석의 가격 [M, V]
@@ -8,41 +9,41 @@ for i in range(n):
     stones.append((m, v))
 # 가격별로 정리하고
 stones.sort(key = lambda x : x[1], reverse=True)
-# 그후 무게 별로
-# print("stones : ",stones)
 # 가방의 최대 무게
-c=[]
+bags=[]
 for i in range(k):
     weight = int(input())
-    c.append(weight)
-# print("c : ",c)
+    bags.append(weight)
+
+# print("stones : ",stones)
 price=0
 sum=0
-# 가격이 제일 비싼 보석을 , 한계 무게가 최대한 타이트한 가방에 넣음
-# 10^10 이 최대 시간이므로 좀 줄여야함
-# 한계 무게가 최대한 타이트한 가방을 찾으려면,,, 
+# print("반복문 시작")
 for st in stones:
-    # 그 가방 배열에 해당 보석의 무게 값을 넣어 정렬시킨 후
-    # 정렬된 배열에서 그 보석 인덱스의 바로 앞 인덱스를 추출함
-    # 그 인덱스의 가방은 사용했으므로 제거 + 보석도 제거
-    c.append(st[0])
-    c.sort()
-    print("c : ",c)
-    idx = c.index(st[0]) # 보석의 인덱스 값
-    print("idx : ",idx)
-    if  k>0:
-        # 만약 같다면 idx 값만 제거하면됨
-        if st[0]==c[idx+1]:
-            c.remove(c[idx])
-            c.remove(c[idx])
-        if st[0]<c[idx+1]:
-            #사용한 가방제거
-            c.remove(c[idx+1])
-            #넣었던 보석제거
-            c.remove(c[idx])
-        price=st[1]
-        k-=1
-        sum+=price
+    tmp_bag=[]
+    heapq.heapify(bags)
+    count=len(bags)
+    # print("bags : ", bags)
+    while count!=0:
+        count-=1
+        bag = heapq.heappop(bags)
+        # print("bag : ", bag)
+        # 처음으로 가방에 보석이 들어가면 그 가방은 다시 쓰지 않고 반복문 나옴
+        if bag>=st[0]:
+            sum+=st[1]
+            # 담지 못했던 가방들을 다시 bags 배열에 넣음
+            bags = bags + tmp_bag
+            break
+        # 가방이 보석을 담지 못한다면
+        # 다시 꺼낸 가방을 넣음
+        else:
+            if count==0:
+                bags=tmp_bag
+                break
+            tmp_bag.append(bag)
+        # 모든 가방을 다 확인해 봤다면 반복문 나옴
+        
+    # print("----")
         
 print(sum)
 
