28bcd912ee1eaa61abe66c416b03dedcc2545d2a
kwon-yongjun <dydwns2397@gmail.com>
2023-03-15 15:50:06 +0900

    repeat 710_Greedy / 1080번 행렬

--- PYTHON/BOJ/710_Greedy/problem_4.py
+++ PYTHON/BOJ/710_Greedy/problem_4.py
@@ -1,33 +1,44 @@
-# 행렬
-n, m = map(int,input().split())
+# 1080번 행렬
+# n x m 행렬
+n, m = map(int, input().split())
+# 행렬 a 와 행렬 b
 a=[]
+for i in range(n):
+    tmp = list(map(int, input()))
+    a.append(tmp)
 b=[]
 for i in range(n):
-    arr=list(map(int,input()))
-    a.append(arr)
-for j in range(n):
-    arr=list(map(int,input()))
-    b.append(arr)
+    tmp = list(map(int, input()))
+    b.append(tmp)
+# a 에서 b 로 바꿀 수 있다면 몇번 뒤집어야하는지 출력
+# 바꿀 수 없다면 -1 출력
+
+# 연산 - 3x3 크기의 부분 행렬에 있는 모든 원소를 뒤집는다
+
+# 첫번째 행부터 같은지 안같은지 확인 한후 
+# 안같으면 연산 수행
+# print("a:")
+# for i in range(n):
+#     print(a[i])
+# print("b:")
+# for i in range(n):
+#     print(b[i])
 count=0
-
-def change(i,j,array):
-    for x in range(i,i+3):
-        for y in range(j,j+3):
-            if array[x][y]==0:
-                array[x][y]=1
-            else:
-                array[x][y]=0
-
-# 값이 달라진 좌표를 기록하고
 for i in range(n-2):
     for j in range(m-2):
         if a[i][j]!=b[i][j]:
-            change(i,j,a)
+            # 연산 수행
             count+=1
-
+            for t in range(i,i+3):
+                for k in range(j,j+3):
+                    a[t][k]=1-a[t][k]
+# print("after a ")
+# for i in range(n):
+#     print(a[i])
 for i in range(n):
     for j in range(m):
         if a[i][j]!=b[i][j]:
             count=-1
-print(count)
+            break
 
+print(count)
--- PYTHON/BOJ/710_Greedy/repeat.py
+++ PYTHON/BOJ/710_Greedy/repeat.py
@@ -1,18 +1,44 @@
-# 11399번  ATM
-# n : 사람 수
-n = int(input())
-# 각 사람이 인술하는데 걸리는 시간
-p = list(map(int, input().split()))
-
-# 각 사람의 돈을 인출 하는데 필요한 시간의 합의 최솟값
-
-# 인출 시간을 오름 차순
-p.sort()
-# 더한 값을 배열에 저장
-ans=0
-sum=0
+# 1080번 행렬
+# n x m 행렬
+n, m = map(int, input().split())
+# 행렬 a 와 행렬 b
+a=[]
 for i in range(n):
-    sum+=p[i]
-    ans+=sum
-print(ans)
-# 그 배열의 총합 구함
+    tmp = list(map(int, input()))
+    a.append(tmp)
+b=[]
+for i in range(n):
+    tmp = list(map(int, input()))
+    b.append(tmp)
+# a 에서 b 로 바꿀 수 있다면 몇번 뒤집어야하는지 출력
+# 바꿀 수 없다면 -1 출력
+
+# 연산 - 3x3 크기의 부분 행렬에 있는 모든 원소를 뒤집는다
+
+# 첫번째 행부터 같은지 안같은지 확인 한후 
+# 안같으면 연산 수행
+# print("a:")
+# for i in range(n):
+#     print(a[i])
+# print("b:")
+# for i in range(n):
+#     print(b[i])
+count=0
+for i in range(n-2):
+    for j in range(m-2):
+        if a[i][j]!=b[i][j]:
+            # 연산 수행
+            count+=1
+            for t in range(i,i+3):
+                for k in range(j,j+3):
+                    a[t][k]=1-a[t][k]
+# print("after a ")
+# for i in range(n):
+#     print(a[i])
+for i in range(n):
+    for j in range(m):
+        if a[i][j]!=b[i][j]:
+            count=-1
+            break
+
+print(count)
