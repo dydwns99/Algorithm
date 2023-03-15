b004a8e9d893f96e1b64f3686fa89d9af5c1ee05
kwon-yongjun <dydwns2397@gmail.com>
2023-03-15 17:19:39 +0900

    repeat 710_Greedy / 2138번 전구와 스위치

--- PYTHON/BOJ/710_Greedy/problem_5.py
+++ PYTHON/BOJ/710_Greedy/problem_5.py
@@ -1,44 +1,53 @@
-# 2138번 전구와 스위치
+# 2138번  전구와 스위치
+# n : 스위치 및 전구의 개수
+n = int(input())
+# 전구의 현재 상태  -> 0 - 켜져있는   1 - 꺼져있는
+a = list(map(int,input()))
+# 전구의 나중 상태 (만들고자하는)
+b = list(map(int,input()))
+# 최소 몇번 눌러야 a에서 b 로 바뀌는지 횟수 출력, 불가능하면 -1 출력
+# i 번 스위치 누르면 i-1, i, i+1 이 바뀜
 
-# 얕은 복사(shallow copy) - a is swt 는 False 지만 , 만약 a안에 객체가 있다면 그 객체들은 메모리 값이 같음
-# 깊은 복사(deep copy) - import copy     copy.deepcopy() -> 내부 객체까지 메모리 주소 바꿈
+# 맨 왼쪽 수 가 같은 걸 확인하고 다음으로 넘어가야함
 
-
-n=int(input())
-bulb = list(map(int,input()))
-target = list(map(int,input()))
-
-
-def change(A,B):
-    A_copy = A[:]
-    press=0
-    for i in range(1,n):
-        if A_copy[i-1]==B[i-1]:
-            continue
-        press+=1
-        for j in range(i-1,i+2):
-            if j<n:
-                # ... n-3 n-2 n-1, n-2 n-1, n-1 처럼 끝까지 확인하여 바꿀 수 있음
-                A_copy[j]=1-A_copy[j] 
-    if A_copy == B:
-        return press
+# 하지만 첫번째를 누르냐 안누르냐를 결정해야 위 규칙 적용 가능
+a1=a[:]
+a2=a[:]
+ans=[]
+for t in range(2):
+    count=0
+    if t==0:
+        # 첫번째를 이미 누른 후   110 - 001 
+        tmp=a1
+        count+=1
+        tmp[0]=1-tmp[0]
+        tmp[1]=1-tmp[1]
+        # print("누름 ",tmp)
     else:
-        return 1e9
-res = change(bulb, target)
+        tmp=a2 # 첫번째 안누를때
+        # print("안누름 ",tmp)
+    # 1,2,3,...,n-3,n-2,n-1
+    for i in range(1,n):
+        if tmp[i-1]==b[i-1]:
+            continue
+        else:
+            count+=1
+            for j in range(i-1,i+2):
+                if j<n:
+                    tmp[j]=1-tmp[j]
+    # print("after ",tmp)
+    for j in range(n):
+        if tmp[j]!=b[j]:
+            count=-1
+            break
+    if count!=-1:
+        ans.append(count)
 
-bulb[0]=1-bulb[0]
-bulb[1]=1-bulb[1]
-
-res = min(res, change(bulb,target)+1)
-
-if res!=1e9:
-    print(res)
+if ans:
+    print(min(ans))
 else:
-    print(-1)
+    print("-1")
 
-   
-
-
-
+ 
 
 
--- PYTHON/BOJ/710_Greedy/repeat.py
+++ PYTHON/BOJ/710_Greedy/repeat.py
@@ -1,44 +1,53 @@
-# 1080번 행렬
-# n x m 행렬
-n, m = map(int, input().split())
-# 행렬 a 와 행렬 b
-a=[]
-for i in range(n):
-    tmp = list(map(int, input()))
-    a.append(tmp)
-b=[]
-for i in range(n):
-    tmp = list(map(int, input()))
-    b.append(tmp)
-# a 에서 b 로 바꿀 수 있다면 몇번 뒤집어야하는지 출력
-# 바꿀 수 없다면 -1 출력
+# 2138번  전구와 스위치
+# n : 스위치 및 전구의 개수
+n = int(input())
+# 전구의 현재 상태  -> 0 - 켜져있는   1 - 꺼져있는
+a = list(map(int,input()))
+# 전구의 나중 상태 (만들고자하는)
+b = list(map(int,input()))
+# 최소 몇번 눌러야 a에서 b 로 바뀌는지 횟수 출력, 불가능하면 -1 출력
+# i 번 스위치 누르면 i-1, i, i+1 이 바뀜
 
-# 연산 - 3x3 크기의 부분 행렬에 있는 모든 원소를 뒤집는다
+# 맨 왼쪽 수 가 같은 걸 확인하고 다음으로 넘어가야함
 
-# 첫번째 행부터 같은지 안같은지 확인 한후 
-# 안같으면 연산 수행
-# print("a:")
-# for i in range(n):
-#     print(a[i])
-# print("b:")
-# for i in range(n):
-#     print(b[i])
-count=0
-for i in range(n-2):
-    for j in range(m-2):
-        if a[i][j]!=b[i][j]:
-            # 연산 수행
+# 하지만 첫번째를 누르냐 안누르냐를 결정해야 위 규칙 적용 가능
+a1=a[:]
+a2=a[:]
+ans=[]
+for t in range(2):
+    count=0
+    if t==0:
+        # 첫번째를 이미 누른 후   110 - 001 
+        tmp=a1
+        count+=1
+        tmp[0]=1-tmp[0]
+        tmp[1]=1-tmp[1]
+        # print("누름 ",tmp)
+    else:
+        tmp=a2 # 첫번째 안누를때
+        # print("안누름 ",tmp)
+    # 1,2,3,...,n-3,n-2,n-1
+    for i in range(1,n):
+        if tmp[i-1]==b[i-1]:
+            continue
+        else:
             count+=1
-            for t in range(i,i+3):
-                for k in range(j,j+3):
-                    a[t][k]=1-a[t][k]
-# print("after a ")
-# for i in range(n):
-#     print(a[i])
-for i in range(n):
-    for j in range(m):
-        if a[i][j]!=b[i][j]:
+            for j in range(i-1,i+2):
+                if j<n:
+                    tmp[j]=1-tmp[j]
+    # print("after ",tmp)
+    for j in range(n):
+        if tmp[j]!=b[j]:
             count=-1
             break
+    if count!=-1:
+        ans.append(count)
 
-print(count)
+if ans:
+    print(min(ans))
+else:
+    print("-1")
+
+ 
+
+
