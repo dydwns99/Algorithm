ea729c9f1bbc891ea278c6baa23d8602cc3fca96
kwon-yongjun <dydwns2397@gmail.com>
2023-03-15 15:02:46 +0900

    repeat 710_Greedy / 11399번 ATM

--- PYTHON/BOJ/710_Greedy/problem_3.py
+++ PYTHON/BOJ/710_Greedy/problem_3.py
@@ -1,25 +1,18 @@
-# 11399 번
+# 11399번  ATM
+# n : 사람 수
+n = int(input())
+# 각 사람이 인술하는데 걸리는 시간
+p = list(map(int, input().split()))
 
-def solution():
-    N = int(input())
-    p = input().split()
-    for i in range(N):
-        p[i]=int(p[i])
-    p.sort()
-    sum=0
-    ans=0
-    arr=[]
-    for i in range(N):
-        sum+=p[i]
-        arr.append(sum)
-        ans+=sum
+# 각 사람의 돈을 인출 하는데 필요한 시간의 합의 최솟값
 
-    return ans
-print(solution())
-
-
-    # for i in range(N):
-    #     arr.append([i+1,p[i]])
-    # print(arr)
-
-solution()
+# 인출 시간을 오름 차순
+p.sort()
+# 더한 값을 배열에 저장
+ans=0
+sum=0
+for i in range(n):
+    sum+=p[i]
+    ans+=sum
+print(ans)
+# 그 배열의 총합 구함
--- PYTHON/BOJ/710_Greedy/repeat.py
+++ PYTHON/BOJ/710_Greedy/repeat.py
@@ -1,23 +1,18 @@
-# 1931번  회의실 배정
-# n: 회의의 수
+# 11399번  ATM
+# n : 사람 수
 n = int(input())
-# 회의 시작시간 , 끝나는 시간
-meetings=[]
-for i in range(n):
-    s,e = map(int, input().split())
-    meetings.append((s,e))
-# 최대 사용할 수 있는 회의의 최대 개수, 회의 겹치면 안됨
+# 각 사람이 인술하는데 걸리는 시간
+p = list(map(int, input().split()))
 
-# (1,4) (5,9) (12,14)
-# 시작이 작은 숫자 중에서 끝나는 시간 가장 작은 수
-meetings.sort(key = lambda x : (x[1],x[0]))
-print(meetings)
-# 첫 회의 끝나고 다음 회의 정할 때 
-# 현재 회의 끝나는 시간 < 다음 회의 시작시간 
-count=1
-end = meetings[0][1]
-for i in range(1, len(meetings)):
-    if end <= meetings[i][0]:
-        count+=1
-        end = meetings[i][1]
-print(count)
+# 각 사람의 돈을 인출 하는데 필요한 시간의 합의 최솟값
+
+# 인출 시간을 오름 차순
+p.sort()
+# 더한 값을 배열에 저장
+ans=0
+sum=0
+for i in range(n):
+    sum+=p[i]
+    ans+=sum
+print(ans)
+# 그 배열의 총합 구함
