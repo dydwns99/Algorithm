a501b01ef3656b960de57a4072db47dd2e6b9b18
kwon-yongjun <dydwns2397@gmail.com>
2023-03-16 15:25:01 +0900

    repeat 710_Greedy / 2109번 순회강연

--- PYTHON/BOJ/710_Greedy/repeat.py
+++ PYTHON/BOJ/710_Greedy/repeat.py
@@ -1,28 +1,25 @@
-# 1285번  동전 뒤집기
-# n x n 
-import sys
+# 2109번  순회강연
+import heapq
+# n : 대학 수
 n = int(input())
-# 동전
-coin=[0] * n
+# d : 강연 기한   p : 강연료
+univ=[]
+mx=0
 for i in range(n):
-    # 한줄을 입력 받고 
-    for j,c in zip(range(n-1,-1,-1), sys.stdin.readline().strip()):
-        if c=='T':
-            coin[i]+= (1<<j)
-            # 000
-            #  00
-            #   1
-            # 위치로만 봐야함
-count=0
-sum=0
-ans=n*n
-# 열에 있는 동전을 다 바꾸는 경우의 수
-for flip in range(1<<n):
-    # coin 의 행 마다
-    for c in coin:
-    # 행 ^ flip 을 하고  1의 개수를 셈
-        count = bin(c ^ flip).count("1")
-    # 그 후 적은 개수를 더함
-        sum+= min(count, n-count)
-    ans=min(ans,sum)
-print(ans)
+    p, d = map(int, input().split())
+    if d>=mx:
+        mx=d
+    univ.append((p, d))
+# 최대로 벌 수 있는 돈, 
+# 기한 날짜 오름차순
+univ.sort(reverse=True) # 강연료 큰 대학 부터 로직 들어감
+ans=0
+visited=[0] * (mx+1) # 방문한 날짜
+for p, d in univ:
+    for i in range(d,0,-1):
+        if not visited[i]: # 방문했다면 기한 내에 방문안한 날짜에 등록
+            visited[i]=1
+            ans+=p
+            break
+    
+
