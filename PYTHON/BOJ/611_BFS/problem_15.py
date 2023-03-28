2c5928df902ab2cf9c9b9fc4aa309a641efcef0d
kwon-yongjun <dydwns2397@gmail.com>
2023-03-28 17:29:18 +0900

    compl 611_BFS / 10026번 적록색약

--- PYTHON/BOJ/611_BFS/problem_15.py
+++ PYTHON/BOJ/611_BFS/problem_15.py
@@ -1,62 +1,44 @@
 # 10026번 적록색약
-import copy
+from collections import deque
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+def can(blind, u, v):
+    if u == v:
+        return True
+    if blind:
+        if u == 'R' and v == 'G':
+            return True
+        if u == 'G' and v == 'R':
+            return True
+    return False
+
+
+def bfs(color,blind):
+    n = len(color)
+    check = [[False]*n for _ in range(n)]
+    ans = 0
+    for i in range(n):
+        for j in range(n):
+            if check[i][j]:
+                continue
+            ans+=1
+            q=deque()
+            q.append((i,j))
+            check[i][j]=True
+
+            while q:
+                x,y = q.popleft()
+                for k in range(4):
+                    nx = dx[k]+x
+                    ny = dy[k]+y
+                    if 0<=nx<n and 0<=ny<n:
+                        if check[nx][ny]:
+                            continue
+                        if can(blind,color[x][y],color[nx][ny]):
+                            check[nx][ny]=True
+                            q.append((nx,ny))
+    return ans
 n = int(input())
-color=[]
-for i in range(n):
-    color.append(list(input()))
-# for c in color:
-#     print(c)
+color = [input() for _ in range(n)]
+print(str(bfs(color, False)) + ' ' + str(bfs(color, True)))
 
-visited=[[False]*n for _ in range(n)]
-visited2=[[False]*n for _ in range(n)]
-
-def dfs(x,y,col):
-    if x<0 or x>=n or y<0 or y>=n:
-        return False
-    if visited[x][y]==False and color[x][y]==col:
-        visited[x][y]=True
-        dfs(x-1,y,col)
-        dfs(x+1,y,col)
-        dfs(x,y-1,col)
-        dfs(x,y+1,col)
-        return True
-    # 0,0 부터 시작
-    # 0,0 의 색이 R이라면 
-    # 상하좌우를 살펴가며 R인 타켓을 방문표시함
-    # 다 방문 했으면 count+=1
-count=0
-for i in range(n):
-    for j in range(n):
-        col=color[i][j]
-        if dfs(i,j,col)==True:
-            count+=1
-# print("count : ",count)
-
-color2=copy.deepcopy(color)
-for i in range(n):
-    for j in range(n):
-        if color2[i][j]=='R' or color2[i][j]=='G':
-            color2[i][j]='S'
-# for c in color2:
-#     print(c)
-
-def dfs2(x,y,col):
-    if x<0 or x>=n or y<0 or y>=n:
-        return False
-    if visited2[x][y]==False and color2[x][y]==col:
-        visited2[x][y]=True
-        dfs2(x-1,y,col)
-        dfs2(x+1,y,col)
-        dfs2(x,y-1,col)
-        dfs2(x,y+1,col)
-        return True
-
-count2=0
-for i in range(n):
-    for j in range(n):
-        col=color2[i][j]
-        if dfs2(i,j,col)==True:
-            count2+=1
-# print("count2 : ",count2)
-print(count,count2)
-    
