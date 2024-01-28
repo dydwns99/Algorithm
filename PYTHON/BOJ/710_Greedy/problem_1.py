52a0ac8c2bbead015f926d202eddca5188d9d3bb
kwon-yongjun <dydwns2397@gmail.com>
2024-01-29 06:27:16 +0900

    Greedy 1

--- PYTHON/BOJ/710_Greedy/problem_1.py
+++ PYTHON/BOJ/710_Greedy/problem_1.py
@@ -1,16 +1,34 @@
-# 11047번 동전 0
-# n : 동전의 종류 개수 , k : 가치의 합
+# # 11047번 동전 0
+# n, k = map(int, input().split())
+# coins=[]
+# for i in range(n):
+#     coins.append(int(input()))
+# # k를 만들기 위해 필요한 동전 개수의 최솟값
+# # 가격이 제일 큰 동전부터 다 써보고 초과된다면 그 다음 동전으로 써봄
+# count=0
+# for i in range(n-1,-1,-1):
+#     # 4200//1000
+#     count+=k//coins[i]
+#     if k%coins[i] == 0:      
+#         break
+#     k=k%coins[i]
+# print(count)
+
+# 동전 N 종류
+
 n, k = map(int, input().split())
 coins=[]
 for i in range(n):
     coins.append(int(input()))
-# k를 만들기 위해 필요한 동전 개수의 최솟값
-# 가격이 제일 큰 동전부터 다 써보고 초과된다면 그 다음 동전으로 써봄
-count=0
-for i in range(n-1,-1,-1):
-    # 4200//1000
-    count+=k//coins[i]
-    if k%coins[i] == 0:      
+# k 보다 작은 동전 부터 채워 넣음
+# coins 에서 가장 큰 동전 부터 비교해 나가야됨
+coins.reverse()
+
+num=0
+for c in coins:
+    num += k//c
+    if k % c == 0 :
         break
-    k=k%coins[i]
-print(count)
+    k = k%c
+print(num)
+    
---
+++ PYTHON/Programmers/codingtest_kit/DFS&BFS/퍼즐조각채우기.py
@@ -0,0 +1,14 @@
+# table에 1인 블럭들을(인접하지않는)
+# game_board에 0 인 부분에 넣음
+from collections import deque   
+def solution(game_board, table):
+    # game_board에서 0인 부분 때서
+    # table의 1중 딱 맞는 부분 있으면 game_board의 해당 영역에서 0을 1로 바꿈
+
+    # 맞는거를 어떻게 표현?
+
+    answer = -1
+    return answer
+
+
+
